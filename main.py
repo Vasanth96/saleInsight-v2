3b5fc892f4d7a713fb519475991e68d3887783b4
Vasanth96 <39763563+Vasanth96@users.noreply.github.com>
2023-12-17 20:14:43 +0530

    Update main-1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import openpyxl
 
 st.set_page_config(page_title='Sales Dashboard', page_icon=":chart_increasing:", layout='wide')
 
