e924a93f8506e0e8c043e05ea88b03321aa71cc2
Vasanth96 <39763563+Vasanth96@users.noreply.github.com>
2023-12-17 20:16:30 +0530

    Update main 2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import openpyxl
 
 st.set_page_config(page_title='Sales Dashboard', page_icon=":chart_increasing:", layout='wide')
 
 @st.cache_data
 def get_data_from_excel():
-    df = pd.read_excel(io="supermarkt_sales.xlsx", engine='openpyxl', sheet_name='Sales', skiprows=3, usecols='B:R', nrows=1000)
+    df = pd.read_excel(io="supermarkt_sales.xlsx", sheet_name='Sales', skiprows=3, usecols='B:R', nrows=1000)
     # -- hour column in the dataframe
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
     return df
